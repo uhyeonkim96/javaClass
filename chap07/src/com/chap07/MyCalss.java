bc38181524c072863738d90ad8d144c952af7983
uhyeonkim96 <uhkim96@naver.com>
2024-12-07 16:04:12 +0900

    변경

--- chap07/src/com/chap07/MyCalss.java
+++ chap07/src/com/chap07/MyCalss.java
@@ -2,7 +2,7 @@ package com.chap07;
 
 public class InterfaceMain{
     public static void main(String[] args) {
-        MyClass myClass = new MyClass();
-        myclass.
+//        MyClass myClass = new MyClass();
+//        myclass.
     }
 }
